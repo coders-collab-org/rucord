aede07eba6f1e6645fc870f97cb8e530e2d826d0
Youssef Khalil <38569107+DarkyEG@users.noreply.github.com>
2023-02-24 16:43:52 +0200

    Format all files

--- rucord_api_types/src/macros.rs
+++ rucord_api_types/src/macros.rs
@@ -1,5 +1,9 @@
 macro_rules! to_value {
     ($map:ident, $key:ident) => {
-        serde_json::from_value($map.remove(stringify!($key)).expect(format!("expected `{}` field", stringify!($key)).as_str())).expect("Inavlid field type")
+        serde_json::from_value(
+            $map.remove(stringify!($key))
+                .expect(format!("expected `{}` field", stringify!($key)).as_str()),
+        )
+        .expect("Inavlid field type")
     };
 }
