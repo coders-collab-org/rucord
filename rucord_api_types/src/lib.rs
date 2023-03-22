8bca885c7fccc96418bcc234d579c2cbce2bf596
Youssef Khalil <38569107+DarkyEG@users.noreply.github.com>
2023-03-22 14:29:56 +0200

    refactor: move `routers` to single file

--- rucord_api_types/src/lib.rs
+++ rucord_api_types/src/lib.rs
@@ -5,40 +5,8 @@ pub type Snowflake = String;
 
 pub mod gateway;
 pub mod structures;
+pub mod routes;
 
 pub use gateway::*;
 pub use structures::*;
 
-macro_rules! create_routes {
-    ($name:ident => $ret:literal $($tt:tt)*) => {
-        create_routes! {
-            $name() => $ret
-            $($tt)*
-        }
-    };
-
-    ($name:ident($($param_name:path: $param_ty:ty),* $(,)?) => $ret:literal $($tt:tt)*) => {
-        #[inline(always)]
-        pub fn $name($(param_name: $param_ty),*) -> String {
-            format!($ret)
-        }
-
-
-        create_routes! {
-            $($tt)*
-        }
-
-
-    };
-
-    () => {}
-}
-
-pub mod routes {
-
-    create_routes! {
-        gateway => "/gateway"
-
-        gateway_bot => "/gateway/bot"
-    }
-}
