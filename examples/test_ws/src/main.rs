6a68d7f82fda04024241c7bca751a2bea67a76e5
Youssef Khalil <38569107+DarkyEG@users.noreply.github.com>
2023-03-22 14:27:46 +0200

    test: update ws example

--- examples/test_ws/src/main.rs
+++ examples/test_ws/src/main.rs
@@ -2,20 +2,18 @@ use std::{env, sync::Arc};
 
 use async_trait::async_trait;
 use rucord_api_types::GatewayIntentBits;
-use rucord_rest::{RequestManager, RequestManagerOptions};
+use rucord_rest::RequestManager;
 use rucord_ws::{
-    Result, ShardError, WebSocketEventHandler, WebSocketManager, WebSocketManagerOptions,
+    api_types, Result, ShardError, WebSocketEventHandler, WebSocketManager, WebSocketManagerOptions,
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let token = env::var("BOT_TOKEN").expect("expected BOT_TOKEN env.");
 
-    let rest = Arc::new(
-        RequestManager::new_with_token(RequestManagerOptions::default(), token.clone()).into(),
-    );
+    let rest = Arc::new(RequestManager::new_with_token(Default::default(), token.clone()).into());
 
-    let intents = GatewayIntentBits::MessageContent;
+    let intents = GatewayIntentBits::MessageContent | GatewayIntentBits::Guilds;
 
     let mut ws = WebSocketManager::new(WebSocketManagerOptions {
         token,
@@ -36,6 +34,16 @@ impl WebSocketEventHandler for RawEventHandler {
         println!("{message}")
     }
     async fn shard_error(&self, id: usize, error: &ShardError) {
-        eprintln!("[ERROR] [SHARD {id}] {error}");
+        eprintln!("[ERROR] [SHARD {id}]: {error}");
+    }
+
+    async fn dispatch(&self, id: usize, data: &api_types::DispatchPayload) {
+        println!("[INFO] [SHARD {id}]: new event {data:#?}")
+    }
+    async fn ready(&self, id: usize, data: &api_types::ReadyData) {
+        println!("[INFO] [SHARD {id}]: shard is ready {data:#?}")
+    }
+    async fn resumed(&self, _id: usize) {
+        // NO OP
     }
 }
